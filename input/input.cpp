219b646ce87558e095854dc3bca676ae9e1c5535
Jamil-Aktar-Ifat <jamilaktarifat@hotmail.com>
2025-07-12 22:43:01 +0100

    taking input in c++

---
+++ input/input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int main() 
+{
+    int x;
+    cout << "-------input--------" << endl;
+    cin >> x ;
+    cout << "--------output--------" << endl;
+    cout << x << endl;
+    int y = 65;
+    cout << (char)y << endl;
+
+    return 0;
+}
